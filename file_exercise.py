5e2f14131645caf1b926f02f0a2815bf005e8715
fun cole <fun@cole.li>
2019-11-28 15:14:16 +0000

    and again

--- file_exercise.py
+++ file_exercise.py
@@ -2,11 +2,16 @@ import json
 
 def main():
     with open("cities.json") as cities_file:
-        cities_data = json.load(cities_file)
+        try:
+            cities_data = json.load(cities_file)
 
-        print("Largest cities in the US by population:")
-        for index, entry in enumerate(cities_data):
-            print(f"{index + 1}: {entry['name']} - {entry['pop']}")
+            print("Largest cities in the US by population:")
+            for index, entry in enumerate(cities_data):
+                print(f"{index + 1}: {entry['name']} - {entry['pop']}")
+
+        except json.decoder.JSONDecodeError as error:
+            print("Sorry, there was an error decoding that json file:")
+            print(f"\t {error}")
 
     print("The file is now closed.")
 
